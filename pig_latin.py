27eed89f2f34f15534875033f47ab13a289cd0fa
Reuven Lerner <reuven@lerner.co.il>
2025-07-08 19:45:24 +0300

    gitautosync update 26

--- pig_latin.py
+++ pig_latin.py
@@ -4,4 +4,6 @@ def pl_word(word):
     else:
         return word[1:] + word[0] + 'ay'
 
-        
+for one_word in 'computer apple papaya elephant mango'.split():
+    print(pl_word(one_word))
+    
