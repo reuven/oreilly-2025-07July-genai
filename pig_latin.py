7ef6e98e6f00e0e87a6fc16504bbd3628c6e0879
Reuven Lerner <reuven@lerner.co.il>
2025-07-08 19:48:09 +0300

    gitautosync update 28

--- pig_latin.py
+++ pig_latin.py
@@ -7,4 +7,12 @@ def pl_word(word):
 for one_word in 'computer apple papaya elephant mango'.split():
     print(pl_word(one_word))
 
-def
+def pl_sentence(sentence):
+    output = []
+
+    for one_word in sentence.split():
+        output.append(pl_word(one_word))
+
+    return ' '.join(output)
+
+print(pl_sentence('this is a test sentence for my chatbot class'))
